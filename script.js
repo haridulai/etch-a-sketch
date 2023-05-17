f9f413a77ea266c3eb279410539c761d23e4a160
Harinder Dulai <haridulaix@gmail.com>
2023-05-17 16:14:10 +0100

    generate grid nodes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Etch-a-Sketch");
+
+const container = document.querySelector("#container");
+
+function createGrid(size) {
+    for(let i = 0; i < size**2; i++){
+        const gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid");
+        container.appendChild(gridDiv);
+    }
+}
+
+createGrid(16);
