25db835381f4f46c400aaca1761045c198fe2a2a
Harinder Dulai <haridulaix@gmail.com>
2023-05-22 18:27:02 +0100

    mouseover event + style changes active state

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 console.log("Etch-a-Sketch");
 
-const container = document.querySelector("#container");
+const container = document.querySelector(".etch-a-sketch");
 
 function createGrid(size) {
     for(let i = 0; i < size**2; i++){
         const gridDiv = document.createElement("div");
         gridDiv.classList.add("grid");
+        gridDiv.addEventListener('mouseover', sketchCell);
         container.appendChild(gridDiv);
     }
+}
+
+function sketchCell() {
+    this.classList.add("active");
 }
 
 createGrid(16);
