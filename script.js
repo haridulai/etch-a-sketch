58a340cbbd3dee4f143ed6f4462d485df0e53db3
Harinder Dulai <haridulaix@gmail.com>
2023-05-22 18:45:17 +0100

    generate grid size by user prompt

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 console.log("Etch-a-Sketch");
 
 const container = document.querySelector(".etch-a-sketch");
+const btnSetGrid = document.querySelector(".btn-set-grid");
+
+btnSetGrid.addEventListener("click", askGridSize);
 
 function createGrid(size) {
-    for(let i = 0; i < size**2; i++){
-        const gridDiv = document.createElement("div");
-        gridDiv.classList.add("grid");
-        gridDiv.addEventListener('mouseover', sketchCell);
-        container.appendChild(gridDiv);
-    }
+  clearGrid();
+
+  for (let i = 0; i < size ** 2; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid");
+    gridDiv.addEventListener("mouseover", sketchCell);
+    container.appendChild(gridDiv);
+  }
+}
+
+function clearGrid() {
+  document.querySelectorAll(".grid").forEach((el) => el.remove());
 }
 
 function sketchCell() {
-    this.classList.add("active");
+  this.classList.add("active");
+}
+
+function askGridSize() {
+  let gridSize = prompt("How many squares for the etch-a-sketch grid?");
+  gridSize = parseInt(gridSize);
+
+  if (gridSize && gridSize <= 100) {
+    createGrid(gridSize);
+  } else {
+    alert("Grid size invalid.");
+  }
 }
 
 createGrid(16);
